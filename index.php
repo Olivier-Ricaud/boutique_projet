3c39535efb33d828e7350346e0d01fa1d46fbc60
Etudiant <Etudiant>
2016-12-23 13:04:57 +0100

    experim

--- index.php
+++ index.php
@@ -8,9 +8,9 @@
  <html lang="en">
  <head>
  	<meta charset="UTF-8">
- 	<title>Document</title>
+ 	<title>IndexGithub</title>
  </head>
  <body>
- 	
+ 	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus optio, magni omnis quas a velit maiores eum corporis. Similique cumque tenetur, iste provident ad. Minima magni consequatur, in dolore minus!</p>
  </body>
  </html>
